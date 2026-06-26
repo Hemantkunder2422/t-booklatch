4e5595d1a3e2a65d4ca459af80f2500cb8864900
Hemantkunder2422 <hkunder0@gmail.com>
2026-06-26 23:04:02 +0530

    Revert "added new services"
    This reverts commit 69f7c5c60d6589cf44547c094f3e50d6194d10cc.

--- src/modules/venue/venue.controller.ts
+++ src/modules/venue/venue.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Post, UseGuards } from '@nestjs/common';
 import { VenueService } from './venue.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { RolesGuard } from '../auth/roles.guard';
@@ -8,7 +8,6 @@ import { Role } from '@prisma/client';
 import { CurrentUser } from '../user/current-user.decorator';
 import type { AuthUser } from 'src/types/auth-user.interface';
 import { AddVenueSpace } from './dto/add-venue-space.dto';
-import { UpdateVenueSpace } from './dto/update-venue.dto';
 
 @Controller('venue')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -31,17 +30,5 @@ export class VenueController {
   @Roles(Role.VENUE_ADMIN)
   async removeSpace(@Param("spaceId") spaceId:string, @CurrentUser() user:AuthUser){
     return this.venueService.removeSpace(spaceId,user)
-  }
-
-  @Get('myspaces/:venueId')
-  @Roles(Role.VENUE_ADMIN,Role.VENUE_STAFF)
-  async mySpaces(@Param("spaceId") venueId:string){
-    return this.venueService.mySpaces(venueId)
-  }
-
-  @Patch('myspace/edit/:spaceId')
-  @Roles(Role.VENUE_ADMIN,Role.VENUE_STAFF)
-  async editSpace(@Param("spaceId") spaceId:string ,@Body() dto:UpdateVenueSpace, @CurrentUser() user:AuthUser){
-    return this.venueService.editSpace(dto,spaceId,user)
   }
 }
