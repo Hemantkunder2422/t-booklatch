4be297054fea29958cf565cc4e18235febce409f
Hemant Kunder <hemant.kunder@bixbytessolutions.com>
2026-07-14 17:37:16 +0530

    added Invoice and Receipt model

--- src/modules/bookings/bookings.service.ts
+++ src/modules/bookings/bookings.service.ts
@@ -5,40 +5,40 @@ import { AuthUser } from 'src/types/auth-user.interface';
 
 @Injectable()
 export class BookingsService {
-    constructor(private prisma:PrismaService){}
+  constructor(private prisma: PrismaService) {}
 
-    async create(dto:CreateBookingDto,user:AuthUser){
-        const existingBooking = await this.prisma.booking.findFirst({
-           where: {
-            venueSpaceId: dto.venueSpaceId,
-            bookingDate: dto.bookingDate,
-            slot: dto.slot,
-            bookingStatus: {
-                not: 'CANCELLED',
-            },
-  },
-        })
-        if(existingBooking){
-            throw new ConflictException("Booking already exists")
-        }
-
-        await this.prisma.booking.create(
-            {
-                data:{
-                    venueId:dto.venueId,
-                    venueSpaceId:dto.venueSpaceId,
-                    customerName:dto.customerName,
-                    customerEmail:dto.customerEmail,
-                    customerPhone:dto.customerPhone,
-                    eventName:dto.eventName,
-                    eventType:dto.eventType,
-                    bookingDate:dto.bookingDate,
-                    slot:dto.slot,
-                    pax:dto.pax,
-                    notes:dto.notes
-                }
-            }
-        )
-        return "Booking created"
+  async create(dto: CreateBookingDto, user: AuthUser) {
+    const existingBooking = await this.prisma.booking.findFirst({
+      where: {
+        venueSpaceId: dto.venueSpaceId,
+        bookingDate: dto.bookingDate,
+        startTime: dto.start_time,
+        endTime: dto.end_time,
+        bookingStatus: {
+          not: 'CANCELLED',
+        },
+      },
+    });
+    if (existingBooking) {
+      throw new ConflictException('Booking already exists');
     }
+
+    await this.prisma.booking.create({
+      data: {
+        venueId: dto.venueId,
+        venueSpaceId: dto.venueSpaceId,
+        customerName: dto.customerName,
+        customerEmail: dto.customerEmail,
+        customerPhone: dto.customerPhone,
+        eventName: dto.eventName,
+        eventType: dto.eventType,
+        bookingDate: dto.bookingDate,
+        startTime: dto.start_time,
+        endTime: dto.end_time,
+        pax: dto.pax,
+        notes: dto.notes,
+      },
+    });
+    return 'Booking created';
+  }
 }
